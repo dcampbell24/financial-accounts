2e3c4824a30d90406a06c73c14bbcc2ee8eff2b6
David L. Campbell <dcampbell24@gmail.com>
2023-12-02 22:25:09 -0500

    Move app to its own file.

--- src/accounts.rs
+++ src/accounts.rs
@@ -1,8 +1,6 @@
 //! All of the financial accounts owned by an entity.
 
 use chrono::{offset::Utc, DateTime, Datelike, TimeZone};
-use clap::Parser;
-use iced::{Element, Sandbox};
 use rust_decimal::Decimal;
 use rust_decimal_macros::dec;
 use serde::{Deserialize, Serialize};
@@ -10,13 +8,11 @@ use serde::{Deserialize, Serialize};
 use std::fs::{File, OpenOptions};
 use std::io::{Read, Write};
 use std::path::PathBuf;
-use std::{mem, u64};
+use std::u64;
 
 use crate::account::Account;
-use crate::app::App;
 use crate::error::Error;
-use crate::message::Message;
-use crate::transaction::{Transaction, TransactionToSubmit};
+use crate::transaction::Transaction;
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct Accounts {
@@ -33,7 +29,7 @@ pub enum Screen {
 }
 
 impl Accounts {
-    fn check_monthly(&mut self) {
+    pub fn check_monthly(&mut self) {
         let past = self.checked_up_to;
         let now = Utc::now();
         let day_1 = TimeZone::with_ymd_and_hms(&Utc, now.year(), now.month(), 1, 0, 0, 0).unwrap();
@@ -53,7 +49,7 @@ impl Accounts {
         self.checked_up_to = now;
     }
 
-    fn empty_accounts() -> Self {
+    pub fn empty_accounts() -> Self {
         Self {
             checked_up_to: DateTime::<Utc>::default(),
             inner: Vec::new(),
@@ -115,7 +111,7 @@ impl Accounts {
         total
     }
 
-    fn save_first(file_path: &PathBuf) {
+    pub fn save_first(file_path: &PathBuf) {
         let j = serde_json::to_string_pretty(file_path).unwrap();
         let mut file = OpenOptions::new()
             .write(true)
@@ -125,191 +121,16 @@ impl Accounts {
         file.write_all(j.as_bytes()).unwrap()
     }
 
-    fn save(file_path: &PathBuf) {
+    pub fn save(file_path: &PathBuf) {
         let j = serde_json::to_string_pretty(file_path).unwrap();
         let mut file = File::create(file_path).unwrap();
         file.write_all(j.as_bytes()).unwrap()
     }
 
-    fn load(file_path: &PathBuf) -> Result<Self, Error> {
+    pub fn load(file_path: &PathBuf) -> Result<Self, Error> {
         let mut buf = String::new();
         let mut file = File::open(file_path)?;
         file.read_to_string(&mut buf)?;
         serde_json::from_str(&buf).map_err(|_| Error::Err("bad json".to_string()))
-    }
-}
-
-#[derive(Parser, Debug)]
-#[command(author, version, about, long_about = None)]
-struct Args {
-    /// Name of the file to load
-    #[arg(long, default_value_t = String::new())]
-    load: String,
-
-    /// Name of the new file
-    #[arg(long, default_value_t = String::new())]
-    new: String,
-}
-
-impl Sandbox for App {
-    type Message = Message;
-
-    fn new() -> Self {
-        let args = Args::parse();
-        let screen = Screen::Accounts;
-
-        if !args.load.is_empty() {
-            let path_buf = PathBuf::from(args.load);
-            let mut accounts = Accounts::load(&path_buf).unwrap();
-            accounts.check_monthly();
-            Accounts::save(&path_buf);
-            return App::new(accounts, &path_buf, screen);
-        }
-        if !args.new.is_empty() {
-            let path_buf = PathBuf::from(args.new);
-            let accounts = Accounts::empty_accounts();
-            Accounts::save_first(&path_buf);
-            return App::new(accounts, &path_buf, screen);
-        }
-
-        let path_buf = PathBuf::new();
-        let accounts = Accounts::empty_accounts();
-        let screen = Screen::NewOrLoadFile;
-        App::new(accounts, &path_buf, screen)
-    }
-
-    fn title(&self) -> String {
-        String::from("Fin Stat")
-    }
-
-    fn update(&mut self, message: Message) {
-        let list_monthly = self.list_monthly();
-        let selected_account = self.selected_account();
-
-        match message {
-            Message::NewFile(mut file) => {
-                file.set_extension("json");
-                self.file_picker.current.push(file);
-                let accounts = Accounts::empty_accounts();
-                Accounts::save_first(&self.file_picker.current);
-                self.accounts = accounts;
-                self.screen = Screen::Accounts;
-                return;
-            }
-            Message::LoadFile(file) => {
-                let accounts = Accounts::load(&file);
-                match accounts {
-                    Ok(mut accounts) => {
-                        accounts.check_monthly();
-                        Accounts::save(&file);
-                        self.accounts = accounts;
-                        self.screen = Screen::Accounts;
-                    }
-                    Err(err) => {
-                        self.file_picker.error = format!("{:?}", err);
-                    }
-                }
-                return;
-            }
-            Message::ChangeDir(path_buf) => {
-                self.file_picker.current = path_buf;
-                self.file_picker.error = String::new();
-                return;
-            }
-            Message::ChangeFileName(file) => {
-                self.file_picker.filename = file;
-                self.file_picker.error = String::new();
-                return;
-            }
-            Message::Back => self.screen = Screen::Accounts,
-            Message::ChangeAccountName(name) => self.name = name,
-            Message::ChangeTx(tx) => self.accounts.inner[selected_account.unwrap()].tx.amount = tx,
-            Message::ChangeDate(date) => {
-                self.accounts.inner[selected_account.unwrap()].tx.date = date
-            }
-            Message::ChangeComment(comment) => {
-                self.accounts.inner[selected_account.unwrap()].tx.comment = comment;
-            }
-            Message::ChangeProjectMonths(i) => self.project_months_str = i,
-            Message::ChangeFilterDateYear(date) => {
-                self.accounts.inner[selected_account.unwrap()].filter_date_year = date;
-            }
-            Message::ChangeFilterDateMonth(date) => {
-                self.accounts.inner[selected_account.unwrap()].filter_date_month = date;
-            }
-            Message::Delete(i) => match self.screen {
-                Screen::NewOrLoadFile => {
-                    panic!("Screen::NewOrLoadFile can't be reached here");
-                }
-                Screen::Accounts => {
-                    self.accounts.inner.remove(i);
-                }
-                Screen::Account(j) => {
-                    self.accounts.inner[j].data.remove(i);
-                }
-                Screen::Monthly(j) => {
-                    self.accounts.inner[j].monthly.remove(i);
-                }
-            },
-            Message::NewAccount => self
-                .accounts
-                .inner
-                .push(Account::new(mem::take(&mut self.name))),
-            Message::UpdateAccount(i) => self.accounts.inner[i].name = mem::take(&mut self.name),
-            Message::ProjectMonths => match self.project_months_str.parse() {
-                Ok(i) => {
-                    self.project_months = i;
-                    self.error_str = String::new();
-                }
-                Err(err) => {
-                    let mut msg = "Parse Project Months error: ".to_string();
-                    msg.push_str(&err.to_string());
-                    self.error_str = msg;
-                }
-            },
-            Message::SelectAccount(i) => self.screen = Screen::Account(i),
-            Message::SelectMonthly(i) => self.screen = Screen::Monthly(i),
-            Message::SubmitTx => {
-                let account = &mut self.accounts.inner[selected_account.unwrap()];
-                match account.submit_tx() {
-                    Ok(tx) => {
-                        if list_monthly {
-                            account.monthly.push(tx);
-                        } else {
-                            account.data.push(tx);
-                            account.data.sort_by_key(|tx| tx.date);
-                        }
-                        account.error_str = String::new();
-                        account.tx = TransactionToSubmit::new();
-                    }
-                    Err(err) => {
-                        account.error_str = err;
-                    }
-                }
-            }
-            Message::SubmitFilterDate => {
-                let account = &mut self.accounts.inner[selected_account.unwrap()];
-                match account.submit_filter_date() {
-                    Ok(date) => {
-                        account.filter_date = date;
-                        account.error_str = String::new();
-                    }
-                    Err(err) => {
-                        account.filter_date = DateTime::<Utc>::default();
-                        account.error_str = err;
-                    }
-                }
-            }
-        }
-        Accounts::save(&self.file_path);
-    }
-
-    fn view(&self) -> Element<Message> {
-        match self.screen {
-            Screen::NewOrLoadFile => self.file_picker.view().into(),
-            Screen::Accounts => self.list_accounts().into(),
-            Screen::Account(i) => self.accounts.inner[i].list_transactions().into(),
-            Screen::Monthly(i) => self.accounts.inner[i].list_monthly().into(),
-        }
     }
 }
--- src/app.rs
+++ src/app.rs
@@ -1,8 +1,10 @@
-use std::path::PathBuf;
+use std::{path::PathBuf, mem};
 
+use chrono::{DateTime, Utc};
+use clap::{command, Parser};
 use iced::{
     widget::{button, column, row, text, text_input, Column},
-    Alignment,
+    Alignment, Sandbox, Element,
 };
 use thousands::Separable;
 
@@ -10,8 +12,20 @@ use crate::{
     accounts::{Accounts, Screen},
     file_picker::FilePicker,
     message::Message,
-    PADDING, TEXT_SIZE,
+    PADDING, TEXT_SIZE, account::Account, transaction::TransactionToSubmit,
 };
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Name of the file to load
+    #[arg(long, default_value_t = String::new())]
+    load: String,
+
+    /// Name of the new file
+    #[arg(long, default_value_t = String::new())]
+    new: String,
+}
 
 #[derive(Clone, Debug)]
 pub struct App {
@@ -120,6 +134,169 @@ impl App {
         match self.screen {
             Screen::NewOrLoadFile | Screen::Accounts | Screen::Account(_) => false,
             Screen::Monthly(_) => true,
+        }
+    }
+}
+
+impl Sandbox for App {
+    type Message = Message;
+
+    fn new() -> Self {
+        let args = Args::parse();
+        let screen = Screen::Accounts;
+
+        if !args.load.is_empty() {
+            let path_buf = PathBuf::from(args.load);
+            let mut accounts = Accounts::load(&path_buf).unwrap();
+            accounts.check_monthly();
+            Accounts::save(&path_buf);
+            return App::new(accounts, &path_buf, screen);
+        }
+        if !args.new.is_empty() {
+            let path_buf = PathBuf::from(args.new);
+            let accounts = Accounts::empty_accounts();
+            Accounts::save_first(&path_buf);
+            return App::new(accounts, &path_buf, screen);
+        }
+
+        let path_buf = PathBuf::new();
+        let accounts = Accounts::empty_accounts();
+        let screen = Screen::NewOrLoadFile;
+        App::new(accounts, &path_buf, screen)
+    }
+
+    fn title(&self) -> String {
+        String::from("Fin Stat")
+    }
+
+    fn update(&mut self, message: Message) {
+        let list_monthly = self.list_monthly();
+        let selected_account = self.selected_account();
+
+        match message {
+            Message::NewFile(mut file) => {
+                file.set_extension("json");
+                self.file_picker.current.push(file);
+                let accounts = Accounts::empty_accounts();
+                Accounts::save_first(&self.file_picker.current);
+                self.accounts = accounts;
+                self.screen = Screen::Accounts;
+                return;
+            }
+            Message::LoadFile(file) => {
+                let accounts = Accounts::load(&file);
+                match accounts {
+                    Ok(mut accounts) => {
+                        accounts.check_monthly();
+                        Accounts::save(&file);
+                        self.accounts = accounts;
+                        self.screen = Screen::Accounts;
+                    }
+                    Err(err) => {
+                        self.file_picker.error = format!("{:?}", err);
+                    }
+                }
+                return;
+            }
+            Message::ChangeDir(path_buf) => {
+                self.file_picker.current = path_buf;
+                self.file_picker.error = String::new();
+                return;
+            }
+            Message::ChangeFileName(file) => {
+                self.file_picker.filename = file;
+                self.file_picker.error = String::new();
+                return;
+            }
+            Message::Back => self.screen = Screen::Accounts,
+            Message::ChangeAccountName(name) => self.name = name,
+            Message::ChangeTx(tx) => self.accounts.inner[selected_account.unwrap()].tx.amount = tx,
+            Message::ChangeDate(date) => {
+                self.accounts.inner[selected_account.unwrap()].tx.date = date
+            }
+            Message::ChangeComment(comment) => {
+                self.accounts.inner[selected_account.unwrap()].tx.comment = comment;
+            }
+            Message::ChangeProjectMonths(i) => self.project_months_str = i,
+            Message::ChangeFilterDateYear(date) => {
+                self.accounts.inner[selected_account.unwrap()].filter_date_year = date;
+            }
+            Message::ChangeFilterDateMonth(date) => {
+                self.accounts.inner[selected_account.unwrap()].filter_date_month = date;
+            }
+            Message::Delete(i) => match self.screen {
+                Screen::NewOrLoadFile => {
+                    panic!("Screen::NewOrLoadFile can't be reached here");
+                }
+                Screen::Accounts => {
+                    self.accounts.inner.remove(i);
+                }
+                Screen::Account(j) => {
+                    self.accounts.inner[j].data.remove(i);
+                }
+                Screen::Monthly(j) => {
+                    self.accounts.inner[j].monthly.remove(i);
+                }
+            },
+            Message::NewAccount => self
+                .accounts
+                .inner
+                .push(Account::new(mem::take(&mut self.name))),
+            Message::UpdateAccount(i) => self.accounts.inner[i].name = mem::take(&mut self.name),
+            Message::ProjectMonths => match self.project_months_str.parse() {
+                Ok(i) => {
+                    self.project_months = i;
+                    self.error_str = String::new();
+                }
+                Err(err) => {
+                    let mut msg = "Parse Project Months error: ".to_string();
+                    msg.push_str(&err.to_string());
+                    self.error_str = msg;
+                }
+            },
+            Message::SelectAccount(i) => self.screen = Screen::Account(i),
+            Message::SelectMonthly(i) => self.screen = Screen::Monthly(i),
+            Message::SubmitTx => {
+                let account = &mut self.accounts.inner[selected_account.unwrap()];
+                match account.submit_tx() {
+                    Ok(tx) => {
+                        if list_monthly {
+                            account.monthly.push(tx);
+                        } else {
+                            account.data.push(tx);
+                            account.data.sort_by_key(|tx| tx.date);
+                        }
+                        account.error_str = String::new();
+                        account.tx = TransactionToSubmit::new();
+                    }
+                    Err(err) => {
+                        account.error_str = err;
+                    }
+                }
+            }
+            Message::SubmitFilterDate => {
+                let account = &mut self.accounts.inner[selected_account.unwrap()];
+                match account.submit_filter_date() {
+                    Ok(date) => {
+                        account.filter_date = date;
+                        account.error_str = String::new();
+                    }
+                    Err(err) => {
+                        account.filter_date = DateTime::<Utc>::default();
+                        account.error_str = err;
+                    }
+                }
+            }
+        }
+        Accounts::save(&self.file_path);
+    }
+
+    fn view(&self) -> Element<Message> {
+        match self.screen {
+            Screen::NewOrLoadFile => self.file_picker.view().into(),
+            Screen::Accounts => self.list_accounts().into(),
+            Screen::Account(i) => self.accounts.inner[i].list_transactions().into(),
+            Screen::Monthly(i) => self.accounts.inner[i].list_monthly().into(),
         }
     }
 }
