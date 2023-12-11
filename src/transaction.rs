1af3bc59f9afc8e41e912c869c89cbc1c1399ef9
David L. Campbell <dcampbell24@gmail.com>
2023-12-10 20:43:59 -0500

    Get rid of default for Transaction and TransactionMonthly

--- src/transaction.rs
+++ src/transaction.rs
@@ -3,7 +3,6 @@
 use ::serde::{Deserialize, Serialize};
 use chrono::{serde::ts_seconds, DateTime, Utc};
 use rust_decimal::Decimal;
-use rust_decimal_macros::dec;
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct Transaction {
@@ -13,41 +12,10 @@ pub struct Transaction {
     pub date: DateTime<Utc>,
 }
 
-impl Transaction {
-    pub fn new() -> Self {
-        Self {
-            amount: dec!(0),
-            comment: String::new(),
-            date: Utc::now(),
-        }
-    }
-}
-
-impl Default for Transaction {
-    fn default() -> Self {
-        Self::new()
-    }
-}
-
 #[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct TransactionMonthly {
     pub amount: Decimal,
     pub comment: String,
-}
-
-impl TransactionMonthly {
-    pub fn new() -> Self {
-        Self {
-            amount: dec!(0),
-            comment: String::new(),
-        }
-    }
-}
-
-impl Default for TransactionMonthly {
-    fn default() -> Self {
-        Self::new()
-    }
 }
 
 impl From<Transaction> for TransactionMonthly {
