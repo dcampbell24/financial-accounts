579a514e86110015ceaa649ebe919183769864d2
David L. Campbell <dcampbell24@gmail.com>
2023-12-12 23:01:55 -0500

    Give the project a bin/ lib.rs structure.

--- src/bin/fin-stat.rs
+++ src/bin/fin-stat.rs
@@ -1,20 +1,7 @@
-mod account;
-mod accounts;
-mod app;
-mod file_picker;
-mod message;
-mod transaction;
-
-use app::App;
+use fin_stat::app::App;
 use iced::Application;
 
-/// The size of padding.
-const PADDING: u16 = 1;
-const EDGE_PADDING: usize = 4;
-/// The size of text widgets.
-const TEXT_SIZE: u16 = 24;
-
-/// Runs the ledger application.
+/// Runs the Fin Stat application.
 pub fn main() -> Result<(), iced::Error> {
     App::run(iced::Settings {
         window: iced::window::Settings {
---
+++ src/lib.rs
@@ -0,0 +1,12 @@
+mod account;
+mod accounts;
+pub mod app;
+mod file_picker;
+mod message;
+mod transaction;
+
+/// The size of padding.
+const PADDING: u16 = 1;
+const EDGE_PADDING: usize = 4;
+/// The size of text widgets.
+const TEXT_SIZE: u16 = 24;
