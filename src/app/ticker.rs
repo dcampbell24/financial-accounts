26104b2481b16075bcdc8a143277df5789992851
David Campbell <dcampbell24@gmail.com>
2024-07-02 17:54:26 -0400

    Make getting the ohlc more generic.

--- src/app/ticker.rs
+++ src/app/ticker.rs
@@ -21,11 +21,11 @@ impl Ticker {
         }
     }
 
-    pub fn get_bitcoin_ohlc(&self) -> Result<(), Box<dyn Error>> {
+    pub fn get_ohlc_untyped(&self, name: &str) -> Result<String, Box<dyn Error>> {
         let url = Url::parse_with_params(
             URL_KRAKEN_OHLC,
             &[
-                ("pair", "XBTUSD"),
+                ("pair", name),
                 // A day.
                 ("interval", "1440"),
                 ("since", &Utc::now().timestamp().to_string()),
@@ -33,54 +33,79 @@ impl Ticker {
         )?;
 
         let response = self.http_client.get(url).send()?;
-        let text = response.text()?;
-        let body: BitCoinResponse = serde_json::from_str(&text)?;
+        let string = response.text()?;
+        Ok(string)
+    }
 
-        if body.error.is_empty() {
-            let ohlc = Ohlc {
-                name: "bitcoin".to_string(),
-                currency: Currency::Usd,
-                date_time: DateTime::from_timestamp(body.result.bitcoin_usd[0][0].take_i64(), 0)
-                    .unwrap(),
-                open: Decimal::from_str(&body.result.bitcoin_usd[0][1].clone().take_string())?,
-                high: Decimal::from_str(&body.result.bitcoin_usd[0][2].clone().take_string())?,
-                low: Decimal::from_str(&body.result.bitcoin_usd[0][3].clone().take_string())?,
-                close: Decimal::from_str(&body.result.bitcoin_usd[0][4].clone().take_string())?,
-                vwap: Decimal::from_str(&body.result.bitcoin_usd[0][5].clone().take_string())?,
-                volume: Decimal::from_str(&body.result.bitcoin_usd[0][6].clone().take_string())?,
-                count: body.result.bitcoin_usd[0][7].take_i64(),
-            };
-            println!("{ohlc}");
-            Ok(())
-        } else {
-            Err(Box::new(OhlcError { errors: body.error }))
+    pub fn get_bitcoin_ohlc(&self) -> Result<Ohlc, Box<dyn Error>> {
+        let name = "XBTUSD".to_string();
+        let string = self.get_ohlc_untyped(&name)?;
+        let response: BitCoinResponse = serde_json::from_str(&string)?;
+        to_ohlc(name, Box::new(response))
+    }
+}
+
+fn to_ohlc(name: String, response: Box<dyn OhlcResponse>) -> Result<Ohlc, Box<dyn Error>> {
+    let errors = response.errors();
+    let result = response.result();
+
+    if errors.errors.is_empty() {
+        let ohlc = Ohlc {
+            name: name.to_string(),
+            currency: Currency::Usd,
+            date_time: DateTime::from_timestamp(result.ohlc[0][0].take_i64(), 0).unwrap(),
+            open: Decimal::from_str(&result.ohlc[0][1].clone().take_string())?,
+            high: Decimal::from_str(&result.ohlc[0][2].clone().take_string())?,
+            low: Decimal::from_str(&result.ohlc[0][3].clone().take_string())?,
+            close: Decimal::from_str(&result.ohlc[0][4].clone().take_string())?,
+            vwap: Decimal::from_str(&result.ohlc[0][5].clone().take_string())?,
+            volume: Decimal::from_str(&result.ohlc[0][6].clone().take_string())?,
+            count: result.ohlc[0][7].take_i64(),
+        };
+        println!("{ohlc}");
+        Ok(ohlc)
+    } else {
+        Err(Box::new(errors))
+    }
+}
+
+trait OhlcResponse {
+    fn errors(&self) -> OhlcErrors;
+    fn result(&self) -> OhlcVec;
+}
+
+impl OhlcResponse for BitCoinResponse {
+    fn errors(&self) -> OhlcErrors {
+        OhlcErrors {
+            errors: self.error.clone(),
+        }
+    }
+
+    fn result(&self) -> OhlcVec {
+        OhlcVec {
+            ohlc: self.result.ohlc.clone(),
+            last: self.result.last,
         }
     }
 }
 
+// fixme: make generic
 #[derive(Clone, Debug, Serialize, Deserialize)]
 struct BitCoinResponse {
     error: Vec<String>,
     result: BitCoinOhlcVec,
 }
 
-#[derive(Debug)]
-struct OhlcError {
-    errors: Vec<String>,
+#[derive(Clone, Debug, Serialize, Deserialize)]
+struct OhlcVec {
+    ohlc: Vec<Vec<IntOrString>>,
+    last: u64,
 }
-
-impl Display for OhlcError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> Result<(), std::fmt::Error> {
-        writeln!(f, "{:#?}", self.errors)
-    }
-}
-
-impl Error for OhlcError {}
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
 struct BitCoinOhlcVec {
     #[serde(rename = "XXBTZUSD")]
-    bitcoin_usd: Vec<Vec<IntOrString>>,
+    ohlc: Vec<Vec<IntOrString>>,
     last: u64,
 }
 
@@ -114,7 +139,7 @@ impl Default for IntOrString {
 }
 
 #[derive(Clone, Debug)]
-struct Ohlc {
+pub struct Ohlc {
     name: String,
     currency: Currency,
     date_time: DateTime<Utc>,
@@ -141,5 +166,19 @@ impl Display for Ohlc {
         writeln!(f, "count: {}", self.count)
     }
 }
+
+#[derive(Debug)]
+struct OhlcErrors {
+    errors: Vec<String>,
+}
+
+impl Display for OhlcErrors {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> Result<(), std::fmt::Error> {
+        writeln!(f, "{:#?}", self.errors)
+    }
+}
+
+impl Error for OhlcErrors {}
+
 // {"error":[],"result":{"GNOUSD":[[1719878400,"286.27","286.88","284.97","284.97","285.78","4.74983692",10]],"last":1719792000}}
 // {"error":[],"result":{"XETHZUSD":[[1719878400,"3438.32","3450.99","3432.20","3444.99","3442.24","357.97391572",651]],"last":1719792000}}
