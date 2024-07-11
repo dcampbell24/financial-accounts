665a6d989d46cd5c6d6124826c968f3e2de426e6
David Campbell <dcampbell24@gmail.com>
2024-07-10 22:56:39 -0400

    Rename to financial-accounts.

--- debian.sh
+++ debian.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-pandoc --standalone --to=man financial-accounts.1.md --output=fin-stat.1
+pandoc --standalone --to=man financial-accounts.1.md --output=financial-accounts.1
 gzip --no-name --best financial-accounts.1
 cargo deb
 rm financial-accounts.1.gz
