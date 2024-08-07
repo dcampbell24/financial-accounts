cffe85672eeb62ba621c2f66f4e937bd1e440321
David Campbell <dcampbell24@gmail.com>
2024-08-06 22:55:08 -0400

    Turn on the struct_field lint.

--- src/lib.rs
+++ src/lib.rs
@@ -5,11 +5,7 @@
 //  clippy::nursery,
     clippy::cargo,
 )]
-#![allow(
-    clippy::similar_names,
-    clippy::struct_field_names,
-    clippy::multiple_crate_versions
-)]
+#![allow(clippy::similar_names, clippy::multiple_crate_versions)]
 
 /// The financial-accounts application.
 pub mod app;
