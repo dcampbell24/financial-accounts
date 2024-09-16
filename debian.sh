13b954cef09a15847261e9d0b1c09d8e625f8022
David Campbell <dcampbell24@gmail.com>
2024-09-16 18:47:20 -0400

    Add a space to debian.sh.

--- debian.sh
+++ debian.sh
@@ -5,6 +5,7 @@ pandoc\
     --variable=section:1\
     --variable=date:2024-07-14\
     --standalone --to=man financial-accounts.1.dj --output=financial-accounts.1
+
 gzip --no-name --best financial-accounts.1
 cargo deb
 rm financial-accounts.1.gz
