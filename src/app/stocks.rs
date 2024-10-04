1c3ceee751f2aeac3f2e363cd3feb8338de7b430
David Campbell <dcampbell24@gmail.com>
2024-10-04 17:02:04 -0400

    Use yahoo_finance_api.

--- src/app/stocks.rs
+++ src/app/stocks.rs
@@ -1,16 +1,9 @@
 use std::fmt;
 
-use anyhow::Context;
-use html5ever::{
-    parse_document,
-    tendril::TendrilSink,
-    tree_builder::{TreeBuilderOpts, TreeSink},
-    ParseOpts,
-};
-use markup5ever_rcdom::{NodeData, RcDom};
 use reqwest::Client;
 use rust_decimal::Decimal;
 use serde::{Deserialize, Serialize};
+use yahoo_finance_api::YahooConnector;
 
 use super::account::transactions::Price;
 
@@ -22,62 +15,12 @@ pub struct StockPlus {
 }
 
 impl Price for StockPlus {
-    async fn get_price(&self, client: &Client) -> anyhow::Result<Decimal> {
-        let resp = client
-            .get(format!("https://finance.yahoo.com/quote/{}/", self.symbol))
-            .send()
-            .await?;
+    async fn get_price(&self, _client: &Client) -> anyhow::Result<Decimal> {
+        let provider = YahooConnector::new()?;
+        let response = provider.get_latest_quotes(&self.symbol, "1d").await?;
+        let quote = response.last_quote()?;
 
-        let text = resp.text().await?;
-
-        let opts = ParseOpts {
-            tree_builder: TreeBuilderOpts {
-                drop_doctype: true,
-                ..Default::default()
-            },
-            ..Default::default()
-        };
-
-        let mut dom = parse_document(RcDom::default(), opts)
-            .from_utf8()
-            .read_from(&mut text.as_bytes())
-            .unwrap();
-
-        let document = dom.get_document();
-
-        let mut children = document.children.clone().into_inner();
-        let mut children_2nd = Vec::new();
-        let mut price = "".into();
-
-        'end: while !children.is_empty() {
-            for child_1st in children {
-                match &child_1st.data {
-                    NodeData::Document
-                    | NodeData::Doctype { .. }
-                    | NodeData::Text { .. }
-                    | NodeData::Comment { .. }
-                    | NodeData::ProcessingInstruction { .. } => {}
-                    NodeData::Element { name, attrs, .. } => {
-                        let name = name.local.to_string();
-                        let attrs = attrs.clone().into_inner();
-                        if name == "fin-streamer" && attrs[0].value == self.symbol.clone().into() {
-                            price = attrs[1].value.clone();
-                            break 'end;
-                        }
-                    }
-                }
-                for child_2nd in child_1st.children.clone().into_inner() {
-                    children_2nd.push(child_2nd);
-                }
-            }
-            children = children_2nd;
-            children_2nd = Vec::new();
-        }
-
-        let price: Decimal = price
-            .parse()
-            .context(format!("Invalid Equity: {}", self.symbol))?;
-        Ok(price)
+        Ok(Decimal::from_f64_retain(quote.close).expect("error converting f64 to Decimal"))
     }
 }
 
