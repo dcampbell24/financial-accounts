aa2cc9a907e3f4737911bb3d066184e2ed269584
David Campbell <dcampbell24@gmail.com>
2024-10-31 00:44:04 -0400

    Move the lint settings to cargo.toml.

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,2 @@
-#![warn(
-    clippy::all,
-//  clippy::restriction,
-    clippy::pedantic,
-//  clippy::nursery,
-    clippy::cargo,
-)]
-#![allow(clippy::multiple_crate_versions)]
-
 /// The financial-accounts application.
 pub mod app;
