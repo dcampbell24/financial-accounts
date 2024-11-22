5dad52d400e2b88c7971bc8f108fa047efb9bf2e
David Campbell <dcampbell24@gmail.com>
2024-11-21 23:19:34 -0500

    Maximize the screen.

--- src/bin/financial-accounts.rs
+++ src/bin/financial-accounts.rs
@@ -2,6 +2,7 @@ use financial_accounts::app::App;
 
 fn main() -> iced::Result {
     iced::application("Financial Accounts", App::update, App::view)
+        .window_size(iced::Size::INFINITY)
         .theme(App::theme)
         .run()
 }
